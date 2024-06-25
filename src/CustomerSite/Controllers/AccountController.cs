5653cf7817fa182e16512bc5fadb27a9062fab87
tom-seculyze <Tomislav@seculyze.com>
2024-06-25 14:37:06 +0200

    new test

--- src/CustomerSite/Controllers/AccountController.cs
+++ src/CustomerSite/Controllers/AccountController.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Client;
-using System.Security.Claims;
 
 namespace Marketplace.SaaS.Accelerator.CustomerSite.Controllers;
 
@@ -20,34 +18,10 @@ public class AccountController : Controller
     /// <returns>
     /// The <see cref="IActionResult" />.
     /// </returns>
-
-    public async Task<IActionResult> SignIn(string returnUrl)
+    public IActionResult SignIn(string returnUrl)
     {
-        var redirectUrl = Url.Action(nameof(HandleToken), "Account", new { returnUrl });
-        return this.Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, OpenIdConnectDefaults.AuthenticationScheme);
+        return this.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
     }
-
-    public async Task<IActionResult> HandleToken(string returnUrl)
-    {
-        var result = await HttpContext.AuthenticateAsync(OpenIdConnectDefaults.AuthenticationScheme);
-        
-        if (result?.Principal != null)
-        {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var idToken = await HttpContext.GetTokenAsync("id_token");
-
-            // Send the token to the frontend. This example returns it as part of the query string.
-            // For security reasons, prefer returning it in a secure cookie or via a secure API.
-            return Redirect($"{returnUrl}?access_token={accessToken}&id_token={idToken}");
-        }
-
-        return RedirectToAction("SignIn");
-    }
-
-    // public IActionResult SignIn(string returnUrl)
-    // {
-    //     return this.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
-    // }
 
     /// <summary>
     /// The SignOut.
